9b452e01b930bdb1a899177d0f8748aee2ebe40b
Lana L <lana.shogo@gmail.com>
2023-09-11 00:54:51 +0300

    commit #3

--- src/Main1.java
+++ src/Main1.java
@@ -2,11 +2,11 @@ public class Main1 {
     public static void main(String[] args) {
 
         int amount = 100;
-        int amountReplenishment = 1001;
+        int amountReplenishment = 1100;
 
         if (amountReplenishment > 1000) {
             int count2 = amountReplenishment / 100;
-            System.out.println("Сумма на счете равна: " + (amount + amountReplenishment) + "." + " Сумма начисленных бонусов: " + count2);
+            System.out.println("Сумма на счете равна: " + (amount + amountReplenishment + count2) + "." + " Сумма начисленных бонусов: " + count2);
         } else {
             System.out.println("Сумма на счете равна: " + (amount + amountReplenishment) + "." + " Совершите покупку свыше 1000 рублей для начисления бонусов.");
         }
